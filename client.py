8482cebb3e5e6a66831e9b627f4a9f4250998b85
gil-x <gino.ladowitch@gmail.com>
2018-04-12 22:36:30 +0200

    Refactored client class

--- client.py
+++ client.py
@@ -1,60 +1,87 @@
-# from datamanager import DataManager
+# from self.datamanager import self.datamanager
 
 class ClientUI:
     """Manage all displays, button, listings."""
 
-    def __init__(self):
-        pass
+    def __init__(self, datamanager):
+        self.datamanager = datamanager
+        self.chosen_category = ""
+        self.ramdom_products = ""
+        self.chosen_product = ""
+
+    def mainmenu(self):
+        print("""
+===================================
+     Welcome in openfoodfacts!
+===================================
+1. Look for good food find_substitutes
+2. See my favorites subsitutes
+        """)
+        choice = self.get_choice("\n> What do you want to do?", 2)
+        if choice == 1:
+            self.ask_category()
+        else:
+            pass
 
     def ask_to_wait(self):
         pass
 
-    def ask_category(self, datamanager):
-        print("Which category do you want to explore?")
-        main_categories = datamanager.get_main_categories()
+    def ask_category(self):
+
+        main_categories = self.datamanager.get_main_categories()
         for main_category in main_categories:
-            print("{}. {}".format(main_category["id"], main_category["category_name"]))
-        # for i in range(1, len(main_categories) + 1):
-        #     print("range:", i)
-        chosen_category = input("your choice:\n")
-        while int(chosen_category) not in range(1, len(main_categories) + 1):
-            print("Sorry, you have to choose a number between 1 and {}.".format(len(main_categories)))
+            print("{}. {}".format(
+                    main_category["id"],
+                    main_category["category_name"],
+                    )
+                )
+        self.chosen_category = self.get_choice(
+            "\n> Which category do you want to explore?",
+            len(main_categories))
+        self.display_product()
 
-        self.display_product(datamanager, chosen_category)
+    def display_product(self):
+        self.ramdom_products = self.datamanager.random_pick(10, self.chosen_category)
+        for id, product in enumerate(self.ramdom_products):
+            print("{}. {} ({} - cat: {})".format(
+                    id + 1,
+                    product["product_name"],
+                    product["grade"],
+                    product["category_id"],
+                    )
+                )
+        self.ask_product_to_substitute()
 
-    def display_product(self, datamanager, chosen_category):
-        # DEBUG:
-        # print("category in display_product:", category)
-        ramdom_products = datamanager.random_pick(10, chosen_category)
-        for id, product in enumerate(ramdom_products):
-            print("{}. {} ({} - cat: {})".format(id + 1, product["product_name"], product["grade"], product["category_id"]))
+    def ask_product_to_substitute(self):
+        self.chosen_product = self.get_choice(
+            "\n> Which product to you want to subsitute?",
+            10)
+        self.chosen_product = self.ramdom_products[self.chosen_product - 1]
+        self.fetch_subsitute()
 
-        self.ask_product_to_substitute(datamanager, chosen_category, ramdom_products)
+    def fetch_subsitute(self):
+        chosen_product_categories = self.chosen_product["categories"]
+        substitutes = self.datamanager.find_substitutes(self.chosen_category, chosen_product_categories)
+        for id, product in enumerate(substitutes):
+            print("{}. {}. {}".format(
+                    id + 1,
+                    product["product_name"],
+                    product["grade"],
+                    )
+                )
 
-    def ask_product_to_substitute(self, datamanager, chosen_category, ramdom_products):
-        print("Which product to you want to subsitute?")
-        chosen_product = input("your choice:\n")
-        while int(chosen_product) not in range(1, len(ramdom_products) + 1):
-            print("Sorry, you have to choose a number between 1 and {}.".format(len(ramdom_products)))
-        print("Your choice is:", ramdom_products[int(chosen_product) - 1])
-        chosen_product = ramdom_products[int(chosen_product) - 1]
-        self.fetch_subsitute(datamanager, chosen_category, chosen_product)
+    def get_choice(self, question, choices_number):
+        print(question)
+        while True:
+            response = input("Your choice: ")
+            try:
+                if int(response) in range(1, choices_number + 1):
+                    return int(response)
+                else:
+                    raise ValueError
+            except (TypeError, ValueError):
+                print("X You have to pick a number between 1 and {}.".format(choices_number))
 
-    def fetch_subsitute(self, datamanager, chosen_category, chosen_product, ):
-        chosen_product_categories = chosen_product["categories"]
-        print(chosen_product_categories)
-        for i in datamanager.find_substitutes(chosen_category, chosen_product_categories):
-            print("{}. {}".format(i["product_name"], i["grade"]))
-        pass
-
-    def get_choice(self, question, response_range):
-        response = input("your choice:\n")
-        try:
-            response = int(response)
-        except TypeError:
-            print("You have to pick a number between")
-        while (response):
-         pass
 
 
     def display_favorites(self):
--- main.py
+++ main.py
@@ -11,24 +11,23 @@ class Main:
     def __init__(self):
         self.database = DataManager(USER, PASSWORD, HOST, DATABASE, CHARSET)
         self.fisher = ApiFisherman()
-        self.client = ClientUI()
+        self.client = ClientUI(self.database)
         self.categories = CATEGORIES
 
+    def update_data(self):
+        self.database.build_db()
+        print("Tables builded")
+        self.database.add_category(self.categories)
+        print("Added categories")
+        while self.categories:
+            category = self.categories.pop(0)
+            print("category:", category)
+            self.database.add_products(self.fisher.fetch_category(category), category)
+
     def run(self):
-        # self.database.build_db()
-        # print("Tables builded")
-        # self.database.add_category(self.categories)
-        # print("Added categories")
-        # while self.categories:
-        #     category = self.categories.pop(0)
-        #     print("category:", category)
-        #     self.database.add_products(self.fisher.fetch_category(category), category)
-
         # self.database.get_main_categories()
-
         # self.database.add_fulltext_index()
-
-        self.client.ask_category(self.database)
+        self.client.mainmenu()
 
 main = Main()
 
